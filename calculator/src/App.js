422f4ae404a6fde4e7604f1586f65833af9629fd
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-12-17 19:22:43 -0800

    trying different styles

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 import Calc2 from './calc2';
-
+import './styles/layout.css';
+//import './styles/style.scss';
 class App extends Component {
   render() {
     return (
+      <Fragment>
+      <h1>Calculator</h1>
       <div className="App">
-        <h1>Calculator</h1>
-        <Calc2/>
+        <Calc2 className="calcOne"/>
+        <Calc2 className="calculator2 calc2"/>
+        <Calc2 className="calc3"/>
+        <Calc2 className="calculator2 calc4"/>
       </div>
+      </Fragment>
     );
   }
 }
