5ffedb5d9f47cb848d8a31c3984c7288e0cf381a
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2018-12-27 13:30:02 -0800

    Updates

---
+++ precache-manifest.360ba4eb95f68186edb446fc1388bbaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "8f56091f904277dfafd9",
+    "url": "/calculator/static/js/main.8f56091f.chunk.js"
+  },
+  {
+    "revision": "84746fe03b1339fbcd32",
+    "url": "/calculator/static/js/1.84746fe0.chunk.js"
+  },
+  {
+    "revision": "8f56091f904277dfafd9",
+    "url": "/calculator/static/css/main.ce623e80.chunk.css"
+  },
+  {
+    "revision": "9c20c2677d43958d8a97db8868b640b6",
+    "url": "/calculator/index.html"
+  }
+];
